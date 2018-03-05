a9303399b3ccc7678719ba955060704c1e4e5419
Diego Dulval <diegodifreitas@gmail.com>
2018-03-05 15:17:13 -0300

    Added hero-app

---
+++ hero-app/src/main/java/com/dulval/demo/heroapp/heroapp/HeroAppApplication.java
@@ -0,0 +1,12 @@
+package com.dulval.demo.heroapp.heroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroAppApplication.class, args);
+	}
+}
