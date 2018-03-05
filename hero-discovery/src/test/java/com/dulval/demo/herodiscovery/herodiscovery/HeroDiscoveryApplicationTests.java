5f40e41f0b75df4a3b1e3cd27c2e14ced56881f4
Diego Dulval <diegodifreitas@gmail.com>
2018-03-05 17:21:44 -0300

    Added dicovery server

---
+++ hero-discovery/src/test/java/com/dulval/demo/herodiscovery/herodiscovery/HeroDiscoveryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dulval.demo.herodiscovery.herodiscovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeroDiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
